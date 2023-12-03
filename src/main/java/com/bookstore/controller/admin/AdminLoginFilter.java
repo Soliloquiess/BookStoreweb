dbe9451c710529c4df773fbe660e188ea352d33e
yacho <cyh1219@naver.com>
2023-12-04 01:13:14 +0900

    feat(yacho)[modify]: modify

--- src/main/java/com/bookstore/controller/admin/AdminLoginFilter.java
+++ src/main/java/com/bookstore/controller/admin/AdminLoginFilter.java
@@ -78,7 +78,6 @@ public class AdminLoginFilter implements Filter {
             // 로그인 페이지로 리다이렉트
             RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
             dispatcher.forward(request, response);
-            System.out.println("test.");
         }
 
     }
